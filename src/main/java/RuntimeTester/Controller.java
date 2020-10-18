086e03cddcba70456de6e47fe7dc92c57a926ebd
Sasha <alexander.aleshchenko@mail.mcgill.ca>
2020-10-18 18:11:07 -0400

    Add base for reflexive method list using Guava, strip out unnecessary stuff!

--- src/main/java/RuntimeTester/Controller.java
+++ src/main/java/RuntimeTester/Controller.java
@@ -1,6 +1,9 @@
 package RuntimeTester;
 
+import com.google.common.reflect.ClassPath;
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.CategoryAxis;
@@ -19,6 +22,8 @@ import javafx.scene.layout.Pane;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -29,7 +34,6 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Controller implements Initializable {
-    private HashTableBenchmark BM;
     private TwitterBenchmark tBM;
     private ScheduledExecutorService scheduledExecutorService;
     //BENCHMARKING
@@ -55,38 +59,12 @@ public class Controller implements Initializable {
     @FXML
     private Button GC_Reset, GC_Help, GC_Refresh;
 
-    //UNIT TESTING
     @FXML
-    private Pane UnitTesting;
-    @FXML
-    private MenuItem UT_RunAll, UT_RunBasicTwitter;
-    @FXML
-    private Button UT_RunBtn;
-    @FXML
-    private TextArea UnitTestTextArea;
     private ArrayList<CheckBox> toggles;
+
     @FXML
-    private TextArea Fun_LastOpRuntime;
-    @FXML
-    private ProgressIndicator Fun_Progress;
-    @FXML
-    private Pane FunDemos;
-    @FXML
-    private Spinner<Integer> Fun_NumOfTweetsAbout, Fun_CommonWordsNum;
-    @FXML
-    private ChoiceBox<String> FunTrendingSelector;
-    @FXML
-    private Button Fun_GoTrends, Fun_GoSong, Fun_GoTweets, Fun_TweetsAboutRand, Fun_CommonWordsRand,
-            Fun_GoWords, Fun_SingAboutRand, fun_Sashaphotolink;
-    @FXML
-    private TextField Fun_TweetsAbout, Fun_SongKeyword;
-    @FXML
-    private TextArea Fun_Output;
-    @FXML
-    private Slider Fun_StopWordFactor;
-    //ALL
-    @FXML
-    private Label BM_Title, UT_Title, FUN_Title;
+    private ButtonBar reflexive_button_area;
+
 
 
     @Override
@@ -96,207 +74,17 @@ public class Controller implements Initializable {
                 GC_J_Put, GC_J_Get, GC_J_Remove, GC_J_Values, GC_J_Keys, GC_Twit_Trending, GC_Twit_Constructor, GC_Twit_ByDate, GC_Twit_ByAuth, GC_Twit_Add,
                 GC_Twit_ConstructorII, GC_Twit_TrendingII));
         addListeners();
-        BM = new HashTableBenchmark();
-        tBM = new TwitterBenchmark(BM.getRand());
-        //playOof();
         initalizeGraph();
     }
 
-    @FXML
-    protected void OpenBenchmarking() {
-        //System.out.println("Switching to Benchmarking View");
-        UnitTesting.setVisible(false);
-        FunDemos.setVisible(false);
-        UT_Title.setOpacity(0.5);
-        FUN_Title.setOpacity(0.5);
-        BM_Title.setOpacity(1);
-        Benchmarking.setVisible(true);
-        //initalizeGraph(0);
-    }
+
 
     @FXML
-    protected void OpenUnitTesting() {
-        //System.out.println("Switching to Unit Testing View");
-        Benchmarking.setVisible(false);
-        FunDemos.setVisible(false);
-        //scheduledExecutorService.shutdownNow();
-        BM_Title.setOpacity(0.5);
-        FUN_Title.setOpacity(0.5);
-        UT_Title.setOpacity(1);
-        UnitTesting.setVisible(true);
-    }
-
-    @FXML
-    protected void OpenFun() {
-        //System.out.println("Switching to fun view");
-        BM_Title.setOpacity(0.5);
-        FUN_Title.setOpacity(1);
-        UT_Title.setOpacity(0.5);
-        UnitTesting.setVisible(false);
-        Benchmarking.setVisible(false);
-        FunDemos.setVisible(true);
-
-    }
 
     private void addListeners() {
-        UT_RunBtn.setOnAction(e -> runAllTests());
-        UT_RunAll.setOnAction(e -> runUnitTests());
-        UT_RunBasicTwitter.setOnAction(e -> runBasicTwitterTest());
         GC_Reset.setOnAction(e -> resetButtons());
         GC_Refresh.setOnAction(e -> initalizeGraph());
         GC_Help.setOnAction(e -> openHelpPage());
-        Fun_GoWords.setOnAction(e -> fun_words(false));
-        Fun_GoSong.setOnAction(e -> fun_sing(false));
-        Fun_GoTrends.setOnAction(e -> fun_trend());
-        Fun_GoTweets.setOnAction(e -> fun_tweets(false));
-        fun_Sashaphotolink.setOnAction(e -> openSashaPhoto());
-        FunTrendingSelector.getItems().addAll(trendOptions);
-        FunTrendingSelector.setValue(trendOptions.get(0));
-        for (CheckBox box : toggles) {
-            box.setOnMouseClicked(e -> initalizeGraph());
-        }
-        Fun_CommonWordsNum.setValueFactory(new SpinnerValueFactory<Integer>() {
-            @Override
-            public void decrement(int i) {
-                setValue(getValue() - i);
-                if (getValue() <= 0) {
-                    setValue(1);
-                }
-                if (getValue() > 200) {
-                    setValue(200);
-                }
-            }
-
-            @Override
-            public void increment(int i) {
-                setValue(getValue() + i);
-                if (getValue() <= 0) {
-                    setValue(1);
-                }
-                if (getValue() > 200) {
-                    setValue(200);
-                }
-            }
-        });
-        Fun_CommonWordsNum.getValueFactory().setValue(5);
-        Fun_NumOfTweetsAbout.setValueFactory(new SpinnerValueFactory<Integer>() {
-            @Override
-            public void decrement(int i) {
-                setValue(getValue() - i);
-                if (getValue() <= 0) {
-                    setValue(1);
-                }
-                if (getValue() > 200) {
-                    setValue(200);
-                }
-            }
-
-            @Override
-            public void increment(int i) {
-                setValue(getValue() + i);
-                if (getValue() <= 0) {
-                    setValue(1);
-                }
-                if (getValue() > 200) {
-                    setValue(200);
-                }
-            }
-        });
-        Fun_NumOfTweetsAbout.getValueFactory().setValue(5);
-        Fun_SingAboutRand.setOnAction(e -> fun_sing(true));
-        Fun_CommonWordsRand.setOnAction(e -> fun_words(true));
-        Fun_TweetsAboutRand.setOnAction(e -> fun_tweets(true));
-    }
-
-    private void openSashaPhoto() {
-        /*if (Desktop.isDesktopSupported()) {
-            try {
-                Desktop.getDesktop().browse(new URI("https://sashaphoto.ca/"));
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            } catch (URISyntaxException e1) {
-                e1.printStackTrace();
-            }
-        }*/
-    }
-
-    private void fun_words(boolean random) {
-        if (random) {
-            Fun_CommonWordsNum.getValueFactory().setValue(1 + BM.getRand().nextInt(49));
-        } else {
-            Fun_Output.setText("");
-            ArrayList<String> words = BM.getRand().nextStopWords(Fun_CommonWordsNum.getValue());
-            for (String s : words) {
-                Fun_Output.appendText(s + "\n");
-            }
-        }
-    }
-
-    private void fun_sing(boolean random) {
-        if (random) {
-            String[] content = BM.getRand().nextContent().split(" ");
-            Fun_SongKeyword.setText(content[content.length / (2)].replace(",", "").strip().trim());
-        } else {
-            try {
-                Fun_Output.setText(BM.getRand().sing(Fun_SongKeyword.getText()));
-            } catch (Exception e) {
-                Fun_Output.setText(BM.getRand().sing());
-            }
-        }
-
-    }
-
-    private void fun_trend() {
-        Fun_Progress.setOpacity(1);
-        Fun_Progress.setProgress(0.1);
-        String choice = FunTrendingSelector.getValue();
-        long StartTime = System.nanoTime();
-        if (choice.equals(trendOptions.get(0))) { //Bee Movie Script
-            Fun_Output.setText(tBM.getRand().nextTrend(0, (int) Fun_StopWordFactor.getValue()));
-        } else if (choice.equals(trendOptions.get(1))) {  //Real Tweets
-            Fun_Output.setText(tBM.getRand().nextTrend(1, (int) Fun_StopWordFactor.getValue()));
-        } else if (choice.equals(trendOptions.get(2))) { //A bunch of songs
-            Fun_Output.setText(tBM.getRand().nextTrend(2, (int) Fun_StopWordFactor.getValue()));
-        } else if (choice.equals(trendOptions.get(3))) {
-            Fun_Output.setText(tBM.getRand().nextTrend(3, (int) Fun_StopWordFactor.getValue()));
-        } else if (choice.equals(trendOptions.get(4))) { //Custom URL
-            //TODO:
-            Fun_Output.setText("URL Trend Finder: Waiting...");
-            final ArrayList<String> URLchoices = new ArrayList<>(Arrays.asList("https://www.mcgill.ca/study/2019-2020/courses/comp-250", "https://stackoverflow.com/questions/240546/remove-html-tags-from-a-string",
-                    "https://twitter.com/explore", "https://en.wikipedia.org/wiki/Python_(programming_language)", "https://en.wikipedia.org/wiki/Special:Random",
-                    "https://www.geeksforgeeks.org/java-util-hashmap-in-java-with-examples/", "https://news.google.com/"));
-            TextInputDialog dialog = new TextInputDialog(URLchoices.get(BM.getRand().nextInt(URLchoices.size() - 1)));
-            dialog.setTitle("URL Prompt");
-            dialog.setHeaderText("Run trend analysis on a custom URL");
-            dialog.setContentText("Enter a valid URL:");
-            Optional<String> result = dialog.showAndWait();
-            result.ifPresent(s -> {
-                try {
-                    Fun_Output.setText(tBM.getRand().nextTrend(s, (int) Fun_StopWordFactor.getValue()));
-                } catch (IOException e) {
-                    Fun_Output.setText(e.getMessage());
-                }
-            });
-        }
-        long endTime = System.nanoTime();
-        Fun_Progress.setProgress(1);
-        Fun_LastOpRuntime.setText((endTime - StartTime) + " nanos");
-        Fun_Progress.setOpacity(0);
-
-    }
-
-    private void fun_tweets(boolean random) {
-        if (random) {
-            Fun_NumOfTweetsAbout.getValueFactory().setValue(1 + BM.getRand().nextInt(49));
-            String[] content = BM.getRand().nextContent().split(" ");
-            Fun_TweetsAbout.setText(content[content.length / (2)].replace(",", "").strip().trim());
-        } else {
-            Tweet[] generated = BM.getRand().nextTweets(Fun_NumOfTweetsAbout.getValue(), true, Fun_TweetsAbout.getText());
-            Fun_Output.setText("");
-            for (Tweet t : generated) {
-                Fun_Output.appendText(t.toString() + "\n");
-            }
-        }
     }
 
     private void openHelpPage() {
@@ -318,34 +106,27 @@ public class Controller implements Initializable {
         initalizeGraph();
     }
 
-    private void runUnitTests() {
-        HashTableUnitTester UT = new HashTableUnitTester(BM.getRand());
-        UnitTestTextArea.setText(UT.runTests());
-    }
+    private void reflexiveGetBenchmarkables() throws IOException {
+        ArrayList<Method> customBenchmarks = new ArrayList<>();
+        for(ClassPath.ClassInfo c : ClassPath.from(this.getClass().getClassLoader()).getAllClasses()){
+            for(Method m : c.load().getMethods()){
+                Annotation[] annotations = m.getAnnotations();
+                if(annotations.length == 0) continue;
+                for(Annotation a : annotations){
+                    if(a instanceof benchmark){
+                        benchmark bm = (benchmark) a;
+                        CheckBox box = new CheckBox(bm.name());
+                        box.setOnAction(new EventHandler<ActionEvent>() {
+                            @Override
+                            public void handle(ActionEvent event) {
+                                //TODO: add this method "m" to a run pool if the box is selected!
+                            }
+                        });
 
-    private void runBasicTwitterTest() {
-        try {
-            UnitTestTextArea.appendText("\n\n\n" + tBM.basicTwitterTest());
-        } catch (NullPointerException e) {
-            tBM = new TwitterBenchmark();
-            try {
-                UnitTestTextArea.appendText("\n\n\n" + tBM.basicTwitterTest());
-            } catch (Exception f) {
-                f.printStackTrace();
-                UnitTestTextArea.appendText("\n\n\n FAILED TO RUN BASIC TWITTER TEST");
+                    }
+                }
             }
-        } catch (Exception e) {
-            UnitTestTextArea.appendText("\n\n\n FAILED TO RUN BASIC TWITTER TEST");
         }
-    }
-
-    private void runAllTests() {
-        UnitTestTextArea.setEditable(false);
-        UnitTestTextArea.setWrapText(true);
-        UnitTestTextArea.setText("Running tests. This will take a while.");
-        runUnitTests();
-        runBasicTwitterTest();
-        UnitTestTextArea.appendText("\n \n \nMore tests coming soon <3");
     }
 
     private void initalizeGraph() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module MultiCOM.main {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires com.google.common;
     opens RuntimeTester to javafx.fxml;
     exports RuntimeTester;
 }
