f9c2a5768c1b9170eb1086706d51b9d0b87de563
Sasha <alexander.aleshchenko@mail.mcgill.ca>
2020-10-19 11:52:15 -0400

    Change default values

--- src/main/java/RuntimeTester/benchmark.java
+++ src/main/java/RuntimeTester/benchmark.java
@@ -4,12 +4,16 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface benchmark {
-    public String name();
-    public String category() default "Custom Methods";
-    public String expectedEfficiency() default "O(?)";
-    public String description() default "";
+    String name();
+
+    String category() default "Other";
+
+    String expectedEfficiency() default "O(?)";
+
+    String description() default "";
 
 }
