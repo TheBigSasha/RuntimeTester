f48c09cfbbff79eaf65621b4194d0af09366fc0b
thebigsasha <sashathethird@gmail.com>
2020-10-23 11:53:10 -0400

    Add method to add classes via List in external API

--- src/main/java/RuntimeTester/Controller.java
+++ src/main/java/RuntimeTester/Controller.java
@@ -206,6 +206,37 @@ public class Controller implements Initializable {
      * Reflects through the module to get all the packages, excluding com.sun. and javafx. packages
      * Adds all @benchmark tagged methods to the tests.
      */
+    public void addBenchmarks(List<Class<?>> testClasses) {
+        try {
+            testClasses.addAll(getClassesForPackage("Tutorial7"));
+            testClasses.addAll(getClassesForPackage("a2"));
+            for(String s : this.getClass().getModule().getPackages()){      //TODO: It does not successfully query the Tutorial7 package?
+                //System.out.println("package named " + s + " was found in module");
+                if(!s.startsWith("javafx.") && !s.startsWith("com.sun.")) { //Ignores things we can't reflect
+                    System.out.println("Querying package " + s);
+                    try {
+                        testClasses.addAll(getClassesForPackage(s));
+                    } catch (Exception ignored) {
+
+                    }
+                }
+            }
+
+
+            for(Class c : testClasses) {
+                System.out.println("Querying class " + c.getName());
+                reflexiveGetBenchmarkables(c);
+            }
+            addReflexiveBenchmarks();
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Reflects through the module to get all the packages, excluding com.sun. and javafx. packages
+     * Adds all @benchmark tagged methods to the tests.
+     */
     private void addBenchmarks() {
         try {
             ArrayList<Class<?>> testClasses = new ArrayList<>();
